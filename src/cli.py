96c9bea739e37435f48b46a4419d65df844b49a2
Alberto Aguilera <drimer.aaj@gmail.com>
2026-04-21 22:49:57 +0100

    feat: report also based on whatsapp filename (#6)

--- src/cli.py
+++ src/cli.py
@@ -9,6 +9,7 @@ from tasks.editors import (
 )
 from tasks.reports import (
     PictureMatcherByExifDateNotInPath,
+    PictureMatcherByExifDateNotInWhatsappFileName,
     PictureMatcherByMissingExifDate,
     find_and_report_imgs,
     report_imgs_without_exif_location,
@@ -29,7 +30,7 @@ def report():
 @click.option("--dir-path", help="Path to the folder with the images")
 def date_not_in_exif(dir_path):
     for img in find_and_report_imgs(
-        dir_path, PictureMatcherByMissingExifDate(), PictureManager()
+        dir_path, (PictureMatcherByMissingExifDate(),), PictureManager()
     ):
         print(img)
 
@@ -45,7 +46,9 @@ def location_not_in_exif(dir_path):
 @click.option("--dir-path", help="Path to the folder with the images")
 def exif_date_not_in_path(dir_path):
     for img in find_and_report_imgs(
-        dir_path, PictureMatcherByExifDateNotInPath(), PictureManager()
+        dir_path,
+        (PictureMatcherByExifDateNotInPath(), PictureMatcherByExifDateNotInWhatsappFileName()),
+        PictureManager()
     ):
         print(img)
 
--- src/tasks/reports.py
+++ src/tasks/reports.py
@@ -1,4 +1,5 @@
 import abc
+from datetime import datetime
 from typing import Generator
 
 from files.manager import PictureManager
@@ -31,6 +32,33 @@ class PictureMatcherByExifDateNotInPath(PictureMatcher):
         )
 
 
+class PictureMatcherByExifDateNotInWhatsappFileName(PictureMatcher):
+    NUMBER_OF_DAYS_WHATSAPP_FILE_NAME_CAN_BE_OFF = 5
+
+    def apply(self, picture: Picture) -> bool:
+        if picture.datetime_taken is None:
+            return False
+
+        example_length = len("IMG-20220101-WA0000.jpg")
+        if len(picture.filename) != example_length:
+            return False
+
+        if not picture.filename.startswith("IMG-") or '-WA' not in picture.filename:
+            return False
+
+        # Allow a time window for WhatsApp file names, as people sometimes forget to share the picture immediately,
+        # and the file name is generated when the picture is shared.
+        filename_date = datetime.strptime(picture.filename[4:12], "%Y%m%d")
+        date_difference = abs((picture.datetime_taken - filename_date).days)
+        if date_difference <= self.NUMBER_OF_DAYS_WHATSAPP_FILE_NAME_CAN_BE_OFF:
+            return False
+
+        start_filename = (
+            f"IMG-{picture.datetime_taken.year}{picture.datetime_taken.month:02d}{picture.datetime_taken.day:02d}"
+        )
+        return not picture.filename.startswith(start_filename)
+
+
 class PictureMatcherByMissingExifDate(PictureMatcher):
     def apply(self, picture: Picture) -> bool:
         return picture.datetime_taken is None
@@ -49,10 +77,10 @@ class PictureMatcherByMissingExifLocation(PictureMatcher):
 
 
 def find_and_report_imgs(
-    path: str, matcher: PictureMatcher, picture_manager: PictureManager
+    path: str, matchers: tuple[PictureMatcher, ...], picture_manager: PictureManager
 ) -> Generator[Picture, None, None]:
     for picture in picture_manager.find_images(path):
-        if matcher.apply(picture):
+        if any(matcher.apply(picture) for matcher in matchers):
             yield picture
 
 
@@ -60,7 +88,7 @@ def report_imgs_without_exif_date(
     path: str, picture_manager: PictureManager
 ) -> Generator[Picture, None, None]:
     return find_and_report_imgs(
-        path, PictureMatcherByMissingExifDate(), picture_manager
+        path, (PictureMatcherByMissingExifDate(),), picture_manager
     )
 
 
@@ -68,5 +96,5 @@ def report_imgs_without_exif_location(
     path: str, picture_manager: PictureManager
 ) -> Generator[Picture, None, None]:
     return find_and_report_imgs(
-        path, PictureMatcherByMissingExifLocation(), picture_manager
+        path, (PictureMatcherByMissingExifLocation(),), picture_manager
     )
