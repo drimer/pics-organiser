aef66081a607c4d01f48d376e946fb0979735aa5
Alberto Aguilera <drimer.aaj@gmail.com>
2026-05-03 15:37:35 +0100

    feat: increase delay of whatsapp pictures to 7 days

--- test/tasks/test_reports.py
+++ test/tasks/test_reports.py
@@ -150,18 +150,18 @@ def test_picture_matcher_by_exif_date_not_in_whatsapp_file_name_does_not_match()
     assert matcher.apply(mock_picture) is False
 
 
-def test_picture_matcher_by_exif_date_not_in_whatsapp_file_name_does_not_match_five_days_in_past():
+def test_picture_matcher_by_exif_date_not_in_whatsapp_file_name_does_not_match_seven_days_in_past():
     mock_picture = mock.Mock()
-    mock_picture.datetime_taken = datetime(2023, 10, 27)
+    mock_picture.datetime_taken = datetime(2023, 10, 25)
     mock_picture.filename = "IMG-20231101-WA0000.jpg"
 
     matcher = PictureMatcherByExifDateNotInWhatsappFileName()
     assert matcher.apply(mock_picture) is False
 
 
-def test_picture_matcher_by_exif_date_not_in_whatsapp_file_name_matches_six_days_in_past():
+def test_picture_matcher_by_exif_date_not_in_whatsapp_file_name_matches_eight_days_in_past():
     mock_picture = mock.Mock()
-    mock_picture.datetime_taken = datetime(2023, 10, 26)
+    mock_picture.datetime_taken = datetime(2023, 10, 24)
     mock_picture.filename = "IMG-20231101-WA0000.jpg"
 
     matcher = PictureMatcherByExifDateNotInWhatsappFileName()
